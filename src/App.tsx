3fd53247582eec599db327f304368afe8ee12b98
Ryan Laptop <delduca.ryan@gmail.com>
2020-01-27 23:00:36 -0800

    Configured firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAazswZwkMIrJX3Xwa9Ph6YrkVN5gdmzRg",
+  authDomain: "tyranid-utility-react.firebaseapp.com",
+  databaseURL: "https://tyranid-utility-react.firebaseio.com",
+  projectId: "tyranid-utility-react",
+  storageBucket: "tyranid-utility-react.appspot.com",
+  messagingSenderId: "316992284721",
+  appId: "1:316992284721:web:8b46e572bd75d02ff44d10",
+  measurementId: "G-MP9Y52CWN1"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const App: React.FC = () => {
   return (
@@ -21,6 +36,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
