0b07ce74f4c0b8577cee5e251367526fa82727a3
Ryan Laptop <delduca.ryan@gmail.com>
2020-02-04 20:19:39 -0800

    Refactored firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import createPalette from "@material-ui/core/styles/createPalette";
 import RouterComponent from "./components/Router";
-
-import * as firebase from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAazswZwkMIrJX3Xwa9Ph6YrkVN5gdmzRg",
-  authDomain: "tyranid-utility-react.firebaseapp.com",
-  databaseURL: "https://tyranid-utility-react.firebaseio.com",
-  projectId: "tyranid-utility-react",
-  storageBucket: "tyranid-utility-react.appspot.com",
-  messagingSenderId: "316992284721",
-  appId: "1:316992284721:web:8b46e572bd75d02ff44d10",
-  measurementId: "G-MP9Y52CWN1"
-};
-
-firebase.initializeApp(firebaseConfig);
+import firebase from "./FirebaseConfig";
 
 const theme = createMuiTheme({
   palette: createPalette({
--- src/components/NavBar/NavDrawer.tsx
+++ src/components/NavBar/NavDrawer.tsx
@@ -7,6 +7,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import HomeIcon from "@material-ui/icons/Home";
+import ListIcon from "@material-ui/icons/List";
 import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles({
@@ -26,11 +27,17 @@ const NavDrawer: FC<{ isOpen: boolean; toggleDrawer: () => void }> = props => {
     <Drawer open={isOpen} onClose={toggleDrawer}>
       <div className={classes.list} onClick={toggleDrawer}>
         <List>
-          <ListItem button component={NavLink} to="/test" key="test">
+          <ListItem button exact component={NavLink} to="/" key="home">
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
-            <ListItemText primary="Test Text"></ListItemText>
+            <ListItemText primary="Home"></ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/test" key="test">
+            <ListItemIcon>
+              <ListIcon />
+            </ListItemIcon>
+            <ListItemText primary="Test"></ListItemText>
           </ListItem>
         </List>
       </div>
